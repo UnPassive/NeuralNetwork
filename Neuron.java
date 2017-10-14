0efd641e884f663e0c3053c337f842c5d1ecf029
Derek Wallace <17050474+PresidentSillyHat@users.noreply.github.com>
2017-10-14 13:49:54 -0600

    Update Neuron.java

--- Neuron.java
+++ Neuron.java
@@ -66,10 +66,32 @@ public class Neuron
 		if(radialBiasActFun)
 		{
 			//activation function here
+                    
+                    //gaussians calc with vectors?
+                    //cluster and change how stuff is passed for RBF?
+                        //exp((-||x-xj||^2)/2sigma^2))
+                        //xj is vector representing the funtion center
+                    
+                        double s=0; //max pairwise distance between clusters(?)/squareroot of number of clusters*2
+                        //large sigma might cause data to become linear
+                        //s could be T times the distance between node and neighbor where T=[1,1.5]
+                        
+                        double c=0; //What would be center? https://link.springer.com/chapter/10.1007/11548706_19 
+                        //does he really want us to create another learning method to find centers? he didnt cover centers in class at all
+                        //http://www.ieee.cz/knihovna/Zhang/Zhang100-ch03.pdf paper on clustering we could use (published officially)
+                        
+                        o=Math.exp(-Math.pow(o-c,2)/(2*Math.pow(s, 2)));
 		}
 		else if(MLFActFun)
 		{
 			//activation function here
+                        //tanh = (2/(1+e^-2x))-1
+                        //or e^(2x)-1/e^(2x)+1
+                        //produce same output
+                    //o=(Math.exp(2*o)-1)/(Math.exp(2*o)+1);
+                    o=(2/(1+Math.exp(-2*o)))-1; 
+                    //double outDeriv=1-Math.pow(o,2); kept hearing we need derivative so here it is just in case
+                    
 		}
 		return o;
 	}
